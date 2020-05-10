7d97b091420cdc894eb255b61a49610ba6e050d2
Damian Klisiewicz <damianklisiewicz@gmail.com>
2020-05-10 19:02:51 +0100

    remove yarn.lock file

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,11 +1,11 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify';
+import Vue from 'vue'
+import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
   icons: {
-    iconfont: 'fa',
-  },
-});
+    iconfont: 'fa'
+  }
+})
