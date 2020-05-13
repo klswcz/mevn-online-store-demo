d8a5512f471af4f1e44359558e11d745235fa948
Damian Klisiewicz <damianklisiewicz@gmail.com>
2020-05-13 21:27:00 +0100

    set loggedIn cookie after user gets logged in

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,7 @@ mongoose.connect('mongodb://localhost:27017/store_demo');
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", function (callback) {
-    console.log("Connection Succeeded");
+    console.log("------Connection Succeeded------");
 });
 
 const User = require('../models/user');
@@ -31,26 +31,26 @@ app.post('/register', [
         return res.status(422).json({errors: errors.array()});
     }
     let email = req.body.email;
-    User.findOne({ email: email }).then(user => {
+    User.findOne({email: email}).then(user => {
         if (user) {
             return res.status(400).send({message: 'User with given email already exists.'});
         }
-        return Bcrypt.hash(req.body.password, 12);
-    }).then(hashedPassword => {
-        let new_user = new User({
-            email: email,
-            password: hashedPassword
-        })
-        new_user.save(error => {
-            if (error) {
-                res.send(error)
-            }
-            res.send({
-                success: true,
-                message: 'User has been registered'
+        return Bcrypt.hash(req.body.password, 12).then(hashedPassword => {
+            let new_user = new User({
+                email: email,
+                password: hashedPassword
+            })
+            new_user.save(error => {
+                if (error) {
+                    res.send(error)
+                }
+                res.send({
+                    success: true,
+                    message: 'User has been registered'
+                })
             })
         })
-    }).catch(error => console.log(error))
+    })
 })
 
 // User log in
@@ -62,18 +62,26 @@ app.post('/login', [
     if (!errors.isEmpty()) {
         return res.status(422).json({errors: errors.array()});
     }
-    try {
-        const user = await User.findOne({email: req.body.email}).exec();
-        if (!user) {
-            return res.status(400).send({message: 'User not found.'});
-        }
-        if (!Bcrypt.compareSync(req.body.password, user.password)) {
-            return res.status(400).send({message: 'Invalid password.'});
-        }
-        res.send({message: "Logged in."});
-    } catch (e) {
-        res.status(500).send(e)
-    }
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({email: email}).then(user => {
+            if (!user) {
+                return res.status(400).send({message: 'User not found.'});
+            }
+            return Bcrypt.compare(password, user.password).then(userFound => {
+                if (userFound) {
+                    res.cookie('loggedIn', 'true')
+                    res.status(200).send({
+                        success: true,
+                        message: 'youre logged in'
+                    })
+                } else {
+                    return res.status(400).send({message: 'Invalid password.'});
+                }
+            }).catch(error => {
+                return res.status(400).send({message: 'Error occurred'});
+            })
+        })
 })
 
 app.listen(process.env.PORT || 8081)
