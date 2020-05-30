112a51863df74459c7a1d075e2eee29063d772a4
Damian Klisiewicz <damianklisiewicz@gmail.com>
2020-05-30 12:55:00 +0100

    add skeleton loaders to the products page

--- client/src/components/Products.vue
+++ client/src/components/Products.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
     <v-container fluid>
-      <v-row>
+      <v-row v-if="showSkeletonLoaders">
+        <div v-for="loader in 12" :key="loader" class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+          <v-skeleton-loader
+            class="mx-4"
+            type="card"
+            elevation="2"
+            loading
+          ></v-skeleton-loader>
+        </div>
+      </v-row>
+      <v-row v-else>
         <div v-for="product in products" :key="product.id" class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
           <v-card class="d-flex mx-4">
             <v-container fluid>
@@ -29,21 +39,23 @@
 </template>
 
 <script>
-import {get as getProducts} from '../services/ProductServices'
+  import {get as getProducts} from '../services/ProductServices'
 
-export default {
-  name: 'Products',
-  data () {
-    return {
-      products: []
+  export default {
+    name: 'Products',
+    data () {
+      return {
+        products: [],
+        showSkeletonLoaders: true
+      }
+    },
+    beforeCreate () {
+      getProducts().then(products => {
+        this.showSkeletonLoaders = false
+        this.products = products
+      })
     }
-  },
-  beforeCreate () {
-    getProducts().then(products => {
-      this.products = products
-    })
   }
-}
 </script>
 
 <style scoped>
