8b0edd1ee7b828c051c63155f5f615f54a0a1961
Damian Klisiewicz <damianklisiewicz@gmail.com>
2020-06-01 21:46:44 +0100

    display more information on the product page

--- client/src/components/Product.vue
+++ client/src/components/Product.vue
@@ -1,7 +1,19 @@
 <template>
-<div>
-  {{ product.name }}
-</div>
+  <div>
+    <v-btn to="/">Go back</v-btn>
+    <h1>{{ product.name }}</h1>
+    <v-container fluid>
+      <v-row>
+        <div class="col-12 col-sm-6 col-lg-3">
+          <v-img :src="product.image_url"></v-img>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-9">
+          <p>Category: {{ product.category_id }}</p>
+          <p>Price: &#163;{{ product.price }}</p>
+        </div>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
